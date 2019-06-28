8ec6c53c4fc594385f6bbe449db7f1d177fb901b
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-06-28 16:31:52 -0700

    Created an example of what a template does by adding it to index method in routes.py.

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,13 @@ from app import app
 @app.route('/index')
 # Home page route.
 def index():
-    return "Hello, World!"
+    user = {'username': 'Ian'}
+    return """
+<html>
+    <head>
+        <title>Home Page - Microblog</title>
+    <head>
+    <body>
+        <h1>Hello, """ + user['username'] + """!</h1>
+    </body>
+</html>"""
