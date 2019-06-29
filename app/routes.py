d3a061ff0f62c5088b2ee7b6366d033435c70c32
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-06-28 20:57:45 -0700

    Created template conditional statement creating a default greeting if no other greeting exists. Added posts functionality, with author referencing user.username and body representing the post.

--- app/routes.py
+++ app/routes.py
@@ -8,4 +8,14 @@ from app import app
 # Home page route.
 def index():
     user = {'username': 'Ian'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='', user=user, posts=posts)
