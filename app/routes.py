f322961b91a98fc2beb78b112f7da9e26690faf3
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-06-28 21:34:04 -0700

    Created config.py file in project's root directory to store all microblog's configurations, created a Config class with a parameter of object that will store SECRET_KEY as an environmental variable.

--- app/routes.py
+++ app/routes.py
@@ -18,4 +18,4 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='', user=user, posts=posts)
+    return render_template('index.html', title='Home', user=user, posts=posts)
