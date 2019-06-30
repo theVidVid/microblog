747ccc44c631017a626f1e514228cb39739ba2af
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-06-29 22:36:18 -0700

    Added login route.

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,8 @@ from flask import render_template
 
 from app import app
 
+from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
@@ -19,3 +21,10 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login')
+# Login page route
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign-in', form=form)
