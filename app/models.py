007fea38794ca2baed5aec1abd7d3ec7a12fc6ab
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-07-01 13:15:29 -0700

    Modified __init__.py file to create a db object, and a migrate object, added models to import statement. Created a User class that includes all the fields included in the user database.

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from app import db
+
+
+class User(db.Model):
+    """Basic class representing a user database and its fields."""
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
