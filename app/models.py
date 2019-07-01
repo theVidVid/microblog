727f3168c416c41cb4d498276865d32551a048e8
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-07-01 14:36:04 -0700

    Created a class named Post in models.py that represents blog post written by users.

--- app/models.py
+++ app/models.py
@@ -1,8 +1,10 @@
+from datetime import datetime
+
 from app import db
 
 
 class User(db.Model):
-    """Basic class representing a user database and its fields."""
+    """Basic class representing a user table and its fields."""
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -10,3 +12,14 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+
+class Post(db.model):
+    """Basic class representing blog posts written by users."""
+    id = db.Column(db.integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
