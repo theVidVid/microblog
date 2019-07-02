d9ae24b1b4c5ff6ac9486749b8407f6c7d1ba4d4
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-07-02 15:49:11 -0700

    Added logout_user to import statement, added logout function that is decorated by the @app.route('/logout).

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,7 @@ from app import app
 
 from app.forms import LoginForm
 
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, logout_user
 
 from app.models import User
 
@@ -41,3 +41,9 @@ def login():
         login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign-in', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
