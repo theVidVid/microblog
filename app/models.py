cb22273cee90e603bb453aac13de14d46ea14f93
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-07-02 15:42:41 -0700

    Added load_user function with @login.user_loader decorator in models.py, imported User class in routes.py and added login and login authentication to login route.

--- app/models.py
+++ app/models.py
@@ -6,6 +6,8 @@ from app import db
 
 from flask_login import UserMixin
 
+from app import login
+
 
 class User(UserMixin, db.Model):
     """Basic class representing a user table and its fields."""
@@ -34,3 +36,8 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
