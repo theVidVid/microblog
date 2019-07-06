48f248292a0e01bf002341e3598961c18d0ad1f1
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-07-05 19:38:14 -0700

    Added a user's profile page route. The route will include the dynamic value of <username>, which will be defined by the user during registration.

--- app/routes.py
+++ app/routes.py
@@ -69,3 +69,15 @@ def register():
         flash('Congratulations, you are a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/user/<username>')
+@login_required
+# A user's profile page route
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'},
+    ]
+    return render_template('user.html', user=user, posts=posts)
