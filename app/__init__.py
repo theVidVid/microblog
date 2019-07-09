c5ceeec2ed55fccbf6d2ea0f8f506aa27e26b380
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-07-09 10:58:32 -0700

    Created a script for the creation of a directory named logs with a log file named microblog that will track errors using the RotatingFileHandler class.

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,9 @@ from flask_login import LoginManager
 
 import logging
 
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
+
+import os
 
 
 app = Flask(__name__)
@@ -35,5 +37,17 @@ if not app.debug:
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
 
 from app import routes, models, errors
