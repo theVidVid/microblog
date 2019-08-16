73ca47d4f99bab2683428b0bcde61abba4771321
Ian Vidaurre <Ianjvidaurre@gmail.com>
2019-08-16 08:47:50 -0700

    Added form for creating post in index.html template, added PostForm class instance to index route, added /explore route, created conditional preventing form from crashing if form fails to render, added link on base template page for /explore route.

--- app/routes.py
+++ app/routes.py
@@ -27,17 +27,9 @@ def index():
         db.session.commit()
         flash('Your post is now live!')
         return redirect(url_for('index'))
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home Page', posts=posts)
+    posts = current_user.followed_posts().all()
+    return render_template('index.html', title='Home Page', form=form,
+                           posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -142,3 +134,10 @@ def unfollow(username):
     db.session.commit()
     flash('You are not following {}.'.format(username))
     return redirect(url_for('user', username=username))
+
+
+@app.route('/explore')
+@login_required
+def explore():
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', title='Explore', posts=posts)
