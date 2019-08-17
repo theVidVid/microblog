22a925a2404e53d276ab65d39520e7b4d9c07fac
Ian Vidaurre <Ianjvidaurre@gmail.com>
2019-08-17 11:31:06 -0700

    Changed posts per page value to 25 instead of 3.

--- config.py
+++ config.py
@@ -17,4 +17,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['ianjvidaurre@gmail.com']
-    POSTS_PER_PAGE = 3
+    POSTS_PER_PAGE = 25
