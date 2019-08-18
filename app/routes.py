5cf6bf5d16c5c2fc40346a1875863ba04f691408
Ian Vidaurre <Ianjvidaurre@gmail.com>
2019-08-18 13:34:44 -0700

    Created /reset_password/<token> route that allows the user to submit a changed password and record changes to app database.

--- app/routes.py
+++ app/routes.py
@@ -8,7 +8,7 @@ from app.models import User, Post
 from datetime import datetime
 from app.email import send_password_reset_email
 from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, \
-    ResetPasswordRequestForm
+    ResetPasswordRequestForm, ResetPasswordForm
 
 
 @app.before_request
@@ -96,6 +96,22 @@ def reset_password_request():
         return redirect(url_for('login'))
     return render_template('reset_password_request.html',
                            title='Reset Password', form=form)
+
+
+@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_password(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    user = User.verify_reset_password_token(token)
+    if not user:
+        return redirect(url_for('index'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        user.set_password(form.password.data)
+        db.sesssion.commit()
+        flash('Your password has ben reset.')
+        return redirect(url_for('login'))
+    return render_template('reset_password.html', form=form)
 
 
 @app.route('/register', methods=['GET', 'POST'])
