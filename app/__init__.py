17eac103811027ae69793e0a09e9eb6ac7ce0326
Ian Vidaurre <Ianjvidaurre@gmail.com>
2019-08-19 19:49:45 -0700

    Updated order of import statements in __init__.py, reworked base.html template with flask-bootstrap styling.

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,13 @@
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 from flask import Flask
-from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
-import logging
-from logging.handlers import SMTPHandler, RotatingFileHandler
-import os
+from config import Config
 
 
 app = Flask(__name__)
