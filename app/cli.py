db3cffd8846cd90c446bef1c35faa106eb2d7b0f
Ian Vidaurre <Ianjvidaurre@gmail.com>
2019-08-24 12:10:35 -0700

    Fixed typo in translate.update() method, added import cli statement to to microblog.py to recognize custom cli flask commands.

--- app/cli.py
+++ app/cli.py
@@ -12,7 +12,7 @@ def translate():
 @translate.command()
 def update():
     """Update all languages."""
-    if os.system('pybabel -F babel.cfg -k _l -o messages.pot .'):
+    if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
         raise RuntimeError('extract command failed')
     if os.system('pybabel update -i messages.pot -d app/translations'):
         raise RuntimeError('update command failed')
