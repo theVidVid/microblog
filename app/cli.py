31c147d36d0579859a8ec7a0dc033b2e9f399f33
Ian Vidaurre <Ianjvidaurre@gmail.com>
2019-08-24 12:02:44 -0700

    Added another sub command, this time utilizing the click.argument decorator and the translate.command decorators to load the selected language code to initialize a new language.

--- app/cli.py
+++ app/cli.py
@@ -1,5 +1,6 @@
 from app import app
 import os
+import click
 
 
 @app.cli.group()
@@ -23,3 +24,15 @@ def compile():
     """Compile all languages."""
     if os.system('pybabel compile -d app/translations'):
         raise RuntimeError('compile command failed')
+
+
+@translate.command()
+@click.argument('lang')
+def init(lang):
+    """Initialize a new language."""
+    if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
+        raise RuntimeError('extract command failed')
+    if os.system(
+            'pybabel init -i messages.pot -d app/translations -l ' + lang):
+        raise RuntimeError('init command failed')
+    os.remove('messages.pot')
