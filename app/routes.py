dda04b8d0b45717f23150fedebd1066c579f3959
Ian Vidaurre <Ianjvidaurre@gmail.com>
2019-08-24 13:01:52 -0700

    Added guess_language import statement to routes.py. Added language to PostForm() class instance, where if language is not recognized or longer than 5 characters, it will equalan empty string. And if it is recognized, it will utilize guess_language to detect a language a post is submitted by a user.

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,7 @@ from app import app, db
 from app.models import User, Post
 from datetime import datetime
 from app.email import send_password_reset_email
+from guess_language import guess_language
 from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, \
     ResetPasswordRequestForm, ResetPasswordForm
 
@@ -26,7 +27,11 @@ def before_request():
 def index():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(body=form.post.data, author=current_user)
+        language = guess_language(form.post.data)
+        if language == 'UNKNOWN' or len(language) > 5:
+            language = ''
+        post = Post(body=form.post.data, author=current_user,
+                    language=language)
         db.session.add(post)
         db.session.commit()
         flash(_('Your post is now live!'))
