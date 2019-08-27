fa999c2e7409fc251de25f37c04f46c79e48bdba
Ian Vidaurre <Ianjvidaurre@gmail.com>
2019-08-27 15:36:01 -0700

    Added import statements to main/forms.py.

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,4 +1,9 @@
-
+from flask import request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import ValidationError, DataRequired, Length
+from flask_babel import _, lazy_gettext as _l
+from app.models import User
 
 
 class EditProfileForm(FlaskForm):
@@ -19,6 +24,5 @@ class EditProfileForm(FlaskForm):
 
 
 class PostForm(FlaskForm):
-    post = TextAreaField(_l('Say something'),
-                         validators=[DataRequired(), Length(min=1, max=140)])
+    post = TextAreaField(_l('Say something'), validators=[DataRequired()])
     submit = SubmitField(_l('Submit'))
