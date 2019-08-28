136ad00db0f10dd7f3d7fc9a775e8df7855222ad
Ian Vidaurre <Ianjvidaurre@gmail.com>
2019-08-27 17:38:53 -0700

    Changed the order of modules in app/__init__.py import statement.

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,9 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
-from config import Config
 from flask_moment import Moment
 from flask_babel import Babel, lazy_gettext as _l
+from config import Config
 
 
 db = SQLAlchemy()
