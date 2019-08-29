6970e3359b4fc1f3ff3dc7256a9b4da8fbfb1e18
Ian Vidaurre <Ianjvidaurre@gmail.com>
2019-08-29 16:21:09 -0700

    Created app/search.py module that has functions for adding to index, removing from index, and querying the index.

--- app/search.py
+++ app/search.py
@@ -3,7 +3,7 @@ from flask import current_app
 
 def add_to_index(index, model):
     if not current_app.elasticsearch:
-        return None
+        return
     payload = {}
     for field in model.__searchable__:
         payload[field] = getattr(model, field)
@@ -12,8 +12,8 @@ def add_to_index(index, model):
 
 def remove_from_index(index, model):
     if not current_app.elasticsearch:
-        return None
-    current_app.elasticsearch.index(index=index, id=model.id)
+        return
+    current_app.elasticsearch.delete(index=index, id=model.id)
 
 
 def query_index(index, query, page, per_page):
@@ -23,5 +23,5 @@ def query_index(index, query, page, per_page):
         index=index,
         body={'query': {'multi_match': {'query': query, 'fields': ['*']}},
               'from': (page - 1) * per_page, 'size': per_page})
-    ids = [int(hit['id']) for hit in search['hits']['hits']]
+    ids = [int(hit['_id']) for hit in search['hits']['hits']]
     return ids, search['hits']['total']['value']
