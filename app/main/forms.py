13129f482dfe2953b05e0f16811bd6092ceba7ed
Ian Vidaurre <Ianjvidaurre@gmail.com>
2019-08-31 16:05:59 -0700

    Created a SearchForm class for submiting search queries as a GET request, with no submitField.

--- app/main/forms.py
+++ app/main/forms.py
@@ -6,6 +6,17 @@ from flask_babel import _, lazy_gettext as _l
 from app.models import User
 
 
+class SearchForm(FlaskForm):
+    q = StringField(_l('Search'), validators=[DataRequired()])
+
+    def __init__(self, *args, **kwargs):
+        if 'formdata' not in kwargs:
+            kwargs['formdata'] = request.args
+        if 'csrf_enabled' not in kwargs:
+            kwargs['csrf_enabled'] = False
+        super(SearchForm, self, *args, **kwargs)
+
+        
 class EditProfileForm(FlaskForm):
     username = StringField(_l('Username'), validators=[DataRequired()])
     about_me = TextAreaField(_l('About me'),
