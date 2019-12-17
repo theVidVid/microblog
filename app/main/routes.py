f61875ea63c129b4002d08cf796b745b57fefe8b
Ian Vidaurre <Ianjvidaurre@gmail.com>
2019-12-17 13:39:54 -0800

    Fixed typo in app/main/routes.

--- app/main/routes.py
+++ app/main/routes.py
@@ -191,8 +191,8 @@ def messages():
         Message.timestamp.desc()).paginate(
             page, current_app.config['POSTS_PER_PAGE'], False)
     next_url = url_for('main.messages', page=messages.next_num) \
-        if messages.has_next() else None
+        if messages.has_next else None
     prev_url = url_for('main.messages', page=messages.prev_num) \
-        if messages.has_prev() else None
+        if messages.has_prev else None
     return render_template('messages.html', messages=messages.items,
                            next_url=next_url, prev_url=prev_url)
